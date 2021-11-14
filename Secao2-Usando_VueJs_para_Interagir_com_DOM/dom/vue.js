77cc6a4580bcc5368e52583db42bd953c41cf7d2
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-14 18:02:48 +0000

    V-once -> Renderizacao uma unica vez

--- Secao2-Usando_VueJs_para_Interagir_com_DOM/dom/vue.js
+++ Secao2-Usando_VueJs_para_Interagir_com_DOM/dom/vue.js
@@ -6,7 +6,10 @@ new Vue({
   },
   methods: {
     saudacao() {
+      this.titulo = "Bom dia";
       return this.titulo;
     },
   },
 });
+
+// v-once - para que a renderizacao ocorra uma unica vez. A partir de entao o VueJs para de monitorar o comportamento daquele elemento.
