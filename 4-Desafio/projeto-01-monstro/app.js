41f132a86bc497b3c1510deef0c086f90796a9ef
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-22 23:05:14 +0000

    Desafio 4 - Mostrando o resultado

--- 4-Desafio/projeto-01-monstro/app.js
+++ 4-Desafio/projeto-01-monstro/app.js
@@ -1,10 +1,14 @@
 new Vue({
   el: "#app",
   data: {
-    playerLife: 100,
+    playerLife: 0,
     monsterLife: 100,
   },
-  computed: {},
+  computed: {
+    hasResult() {
+      return this.playerLife === 0 || this.monsterLife === 0;
+    },
+  },
   methods: {},
   watch: {},
 });
