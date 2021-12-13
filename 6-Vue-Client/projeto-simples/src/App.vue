f129fb56bc4f4a2e8bf2d84a7212dec4451f5848
Andre Lassalvia <andrelassalvia@gmail.com>
2021-12-13 19:27:24 +0000

    Vue client - plugins and others

---
+++ 6-Vue-Client/projeto-simples/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>{{ titulo }}</h1>
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: "Teste!!!",
+    };
+  },
+};
+</script>
+
+<style></style>
