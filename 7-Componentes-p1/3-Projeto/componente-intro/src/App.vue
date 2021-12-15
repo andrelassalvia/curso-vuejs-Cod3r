903b8b26b77a25e861bd1255e830a9748b4ebaff
Andre Lassalvia <andrelassalvia@gmail.com>
2021-12-15 13:59:08 +0000

    Criacao projeto componente-intro

---
+++ 7-Componentes-p1/3-Projeto/componente-intro/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Criacao de um projeto -->
+  <div id="app">
+    <h1>Contador</h1>
+    {{ contador }}
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+};
+</script>
